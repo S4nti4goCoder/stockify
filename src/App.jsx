dad0b0d1bad2d3a3840fe9afb78ab83e6609085e
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-09-01 13:16:58 -0500

    feat: The sections are added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
-import { useState } from "react";
-import "./App.css";
-import { AuthContextProvider } from "./index";
+import styled, { ThemeProvider } from "styled-components";
+import { AuthContextProvider, MyRoutes, Light, Dark } from "./index";
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [themeuse, setTheme] = useState("dark");
+  const theme = themeuse === "light" ? "light" : "dark";
+  const themeStyle = theme === "light" ? Light : Dark;
+
   return (
     <>
-      <AuthContextProvider>
-        <span>Hola desde Context</span>
-      </AuthContextProvider>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <ThemeProvider theme={themeStyle}>
+          <AuthContextProvider>
+            <Container>
+              <section className="ContentSidebar">
+
+              </section>
+              <section className="ContentMenuHam">
+
+              </section>
+              <section className="ContentRoutes">
+
+              </section>
+              <MyRoutes />
+            </Container>
+          </AuthContextProvider>
+        </ThemeProvider>
+      </ThemeContext.Provider>
     </>
   );
 }
+
+const Container = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  background-color: ${({theme}) => theme.bgtotal};
+`;
 
 export default App;
