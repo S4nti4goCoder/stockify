63719f0aca0f3eaf7060454de27e11763f1e4941
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-09-08 22:46:15 -0500

    feat(responsive): Implement responsive layout with sidebar and hamburger menu, and apply dynamic theme styles in HomeTemplate.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import styled, { ThemeProvider } from "styled-components";
 import { AuthContextProvider, MyRoutes, Light, Dark } from "./index";
 import { createContext, useState } from "react";
+import { Device } from "./styles/breackpoints";
 
 export const ThemeContext = createContext(null);
 
@@ -8,23 +9,19 @@ function App() {
   const [themeuse, setTheme] = useState("dark");
   const theme = themeuse === "light" ? "light" : "dark";
   const themeStyle = theme === "light" ? Light : Dark;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <>
       <ThemeContext.Provider value={{ theme, setTheme }}>
         <ThemeProvider theme={themeStyle}>
           <AuthContextProvider>
-            <Container>
-              <section className="ContentSidebar">
-
-              </section>
-              <section className="ContentMenuHam">
-
-              </section>
+            <Container className={sidebarOpen ? "active" : ""}>
+              <section className="ContentSidebar">Sidebar</section>
+              <section className="ContentMenuHam">Menu Hamburguesa</section>
               <section className="ContentRoutes">
-
+                <MyRoutes />
               </section>
-              <MyRoutes />
             </Container>
           </AuthContextProvider>
         </ThemeProvider>
@@ -36,7 +33,34 @@ function App() {
 const Container = styled.main`
   display: grid;
   grid-template-columns: 1fr;
-  background-color: ${({theme}) => theme.bgtotal};
+  background-color: ${({ theme }) => theme.bgtotal};
+  .ContentSidebar {
+    display: none;
+  }
+  .ContentMenuHam {
+    display: block;
+    position: absolute;
+    left: 20px;
+  }
+  @media ${Device.tablet} {
+    grid-template-columns: 65px 1fr;
+    &.active {
+      grid-template-columns: 220px 1fr;
+    }
+    .ContentSidebar {
+      display: initial;
+    }
+    .ContentMenuHam {
+      display: none;
+    }
+  }
+  .ContentRoutes {
+    grid-column: 1;
+    width: 100%;
+    @media ${Device.tablet} {
+      grid-column: 2;
+    }
+  }
 `;
 
 export default App;
