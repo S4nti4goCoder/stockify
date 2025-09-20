e44212135b7a94f3f0ecf7fd8cb7e23c02c63947
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-09-20 16:14:02 -0500

    feat: The hamburger menu toggle button is adapted.

--- src/components/organismos/MenuHam.jsx
+++ src/components/organismos/MenuHam.jsx
@@ -1,19 +1,55 @@
 import styled from "styled-components";
+import { LinksArray, SecondarylinksArray, ToggleTema } from "../../index";
+import { NavLink } from "react-router-dom";
+import { v } from "../../styles/variables";
+import { useState } from "react";
 
 export function MenuHam() {
+  const [click, setClick] = useState(false);
   return (
     <Container>
       <NavBar>
         <section>
-          <HamburgerMenu>
-            <input type="checkbox" id="checkbox" />
-            <label for="checkbox" class="toggle">
+          <HamburgerMenu onClick={() => setClick(!click)}>
+            <label
+              className={click ? "toggle active" : "toggle"}
+              for="checkbox"
+            >
               <div class="bars" id="bar1"></div>
               <div class="bars" id="bar2"></div>
               <div class="bars" id="bar3"></div>
             </label>
           </HamburgerMenu>
         </section>
+        <Menu $click={click.toString()}>
+          {LinksArray.map(({ icon, label, to }) => (
+            <div
+              onClick={() => setClick(!click)}
+              className="LinkContainer"
+              key={label}
+            >
+              <NavLink to={to} className="Links">
+                <div className="Linkicon">{icon}</div>
+                <span>{label}</span>
+              </NavLink>
+            </div>
+          ))}
+          <Divider />
+          {SecondarylinksArray.map(({ icon, label, to }) => (
+            <div
+              className="LinkContainer"
+              key={label}
+              onClick={() => setClick(!click)}
+            >
+              <NavLink to={to} className="Links">
+                <div className="Linkicon">{icon}</div>
+                <span>{label}</span>
+              </NavLink>
+            </div>
+          ))}
+          <ToggleTema />
+          <Divider />
+        </Menu>
       </NavBar>
     </Container>
   );
@@ -49,6 +85,28 @@ const HamburgerMenu = styled.span`
     justify-content: center;
     gap: 10px;
     transition-duration: 0.5s;
+    &.active {
+      transition-duration: 0.5s;
+      transform: rotate(180deg);
+      .bars {
+        position: absolute;
+        transition-duration: 0.5s;
+      }
+      #bar2 {
+        transform: scaleX(0);
+        transition-duration: 0.5s;
+      }
+      #bar1 {
+        width: 100%;
+        transform: rotate(45deg);
+        transition-duration: 0.5s;
+      }
+      #bar3 {
+        width: 100%;
+        transform: rotate(-45deg);
+        transition-duration: 0.5s;
+      }
+    }
   }
 
   .bars {
@@ -66,31 +124,21 @@ const HamburgerMenu = styled.span`
   #bar3 {
     width: 70%;
   }
+`;
 
-  #checkbox:checked + .toggle .bars {
-    position: absolute;
-    transition-duration: 0.5s;
+const Menu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+  z-index: 100;
+  @media screen {
   }
+`;
 
-  #checkbox:checked + .toggle #bar2 {
-    transform: scaleX(0);
-    transition-duration: 0.5s;
-  }
-
-  #checkbox:checked + .toggle #bar1 {
-    width: 100%;
-    transform: rotate(45deg);
-    transition-duration: 0.5s;
-  }
-
-  #checkbox:checked + .toggle #bar3 {
-    width: 100%;
-    transform: rotate(-45deg);
-    transition-duration: 0.5s;
-  }
-
-  #checkbox:checked + .toggle {
-    transition-duration: 0.5s;
-    transform: rotate(180deg);
-  }
+const Divider = styled.div`
+  height: 1px;
+  width: 100%;
+  background: ${(props) => props.theme.bg4};
+  margin: ${() => v.lgSpacing} 0;
 `;
